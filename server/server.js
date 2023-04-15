c5d36e44d0a24eee3b1d3309542147cee06d5bff
Jorge <jpmmpj@gmail.com>
2023-04-15 10:45:09 +0200

    development environment with docker-compose

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
 const productsRoutes = require('./routes/products');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 6005;
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use('/api/products', productsRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
